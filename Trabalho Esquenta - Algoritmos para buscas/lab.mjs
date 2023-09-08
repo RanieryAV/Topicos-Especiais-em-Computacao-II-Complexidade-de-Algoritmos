312ede5fe4e422d9acb7a47d27f188e66e8ac33e
Raniery Alves Vasconcelos <58216614+RanieryAV@users.noreply.github.com>
2023-09-08 11:37:19 -0300

    Paridade com o repo do Jonas (01-09-2023)

--- Trabalho Esquenta - Algoritmos para buscas/lab.mjs
+++ Trabalho Esquenta - Algoritmos para buscas/lab.mjs
@@ -1,5 +1,6 @@
 import measureComplexity from "./modules/measureIt.mjs";
 import loadData from "./modules/loadData.mjs";
+import { generateOrderedNumbers, generateUnorderedNumbers } from "./modules/dataGenerator.mjs"; 
 
 import binarySearch from "./src/binarySearch.mjs";
 import cubicSearch from "./src/cubicSearch.mjs";
@@ -8,34 +9,55 @@ import quadraticSearch from "./src/quadraticSearch.mjs";
 import ternarySearch from "./src/ternarySearch.mjs";
 
 const sizeInstance = 100000000
-const desiredValue=49999995
-const note = `Busca binária no meio - instância de ${sizeInstance} não-ordenada - valor buscado '${desiredValue}'`
+const targetValue = 99999126
+const iterations = 10
+const note = `Busca_Ternária_intância_${sizeInstance}_alvo_${targetValue}`
 
-loadData(`Trabalho Esquenta - Algoritmos para buscas/data/ordered/${sizeInstance}.txt`).then(data =>{
+// loadData(`./data/data-for-search/ordered/${sizeInstance}.txt`).then(data =>{
 
-    function algorithm(){
-        //console.log("\n Busca Quadrática____________")
-        //console.log("Value '"+ desiredValue + "' found at position: " + quadraticSearch(desiredValue, data))
+//     function algorithm(){
 
-        //console.log("\n Busca Cubica____________")
-        //console.log("Value '"+ desiredValue + "' found at position: " + cubicSearch(desiredValue, data))
+//         // console.log("\n Busca Quadrática____________")
+//         // console.log(quadraticSearch(targetValue, data))
+ 
+//         // console.log("\n Busca Cubica____________")
+//         // console.log(cubicSearch(targetValue, data))
+ 
+//         // console.log("\n Busca Sequencial____________")
+//         // console.log(sequentialSearch_v2(targetValue, data))
+        
+//         // console.log("\n Busca Binaria____________")
+//         // console.log(binarySearch(targetValue, data, 0, data.length))
 
-        console.log("\n Busca Binaria____________")
-        console.log("Value '"+ desiredValue + "' found at position: " + binarySearch(desiredValue, data, 0, data.length))
+//         console.log("\n Busca Ternaria____________")
+//         console.log(ternarySearch(targetValue, data))
 
-        //console.log("\n Busca Sequencial____________")
-        //console.log("Value '"+ desiredValue + "' found at position: " + sequentialSearch_v2(desiredValue, data))
+//     }
 
-        // console.log("\n Busca Ternaria____________")
-        // console.log("Value '"+ desiredValue + "' found at position: " + ternarySearch(desiredValue, data))
+//     measureComplexity(algorithm, iterations, `${note}`)
 
-    }
+// });
 
-    measureComplexity(algorithm, 5, `${note}`)
+const data = generateOrderedNumbers(sizeInstance)
+console.log(data)
 
-});
+function algorithm(){
+    // console.log("\n Busca Quadrática____________")
+    // console.log(quadraticSearch(targetValue, data))
 
+    // console.log("\n Busca Cubica____________")
+    // console.log(cubicSearch(targetValue, data))
 
+    // console.log("\n Busca Sequencial____________")
+    // console.log(sequentialSearch_v2(targetValue, data))
+    
+    // console.log("\n Busca Binaria____________")
+    // console.log(binarySearch(targetValue, data, 0, data.length))
 
+    console.log("\n Busca Ternaria____________")
+    console.log(ternarySearch(targetValue, data))
 
+}
+
+measureComplexity(algorithm, iterations, `${note}`)
 
